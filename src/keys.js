279398b9b0114bce2faf5877d2234b28043966c9
Ian <ianhdez2020@gmail.com>
2024-11-24 20:43:47 -0600

    cambio en keys

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,10 @@ const passport = require("passport");
 
 module.exports ={
     database: {
-        host: 'localhost',
-        user:'root',
-        password: 'JU1Pz,98',
-        database: 'database_PSB'
+        host: process.env.DB_HOST ||'localhost' ,
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || 'JU1Pz,98',
+        database: process.env.DB_NAME ||  'database_PSB',
+        port: process.env.DB_PORT || 3306,
     }
 }
